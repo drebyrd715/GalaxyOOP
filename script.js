1cf3bed65f8d8b6a4d0288a73b2444582f4f4ad2
drebyrd715 <drebyrd715@gmail.com>
2023-11-03 14:58:11 -0400

    getting there

--- script.js
+++ script.js
@@ -5,64 +5,84 @@ class Hero {
     this.hull= 20
     this.firepower= 5
     this.accuracy= .7
-    this.isAlive= true
+
 }
 
-    attack(target) {
+    attack(alien) {
         let ranNum = Math.random();
         console.log(`Accuracy is ${ranNum}`);
         if (ranNum < this.accuracy) {
-            console.log(`Got em!`);
-            this.hull = this.hull - this.firepower;
-            console.log(`alien has ${this.hull} hull points left.`);
-            if (this.hull <= 0) {
-                this.isAlive = false;
+            console.log(`Hit enemy ship!`);
+            alien.hull = alien.hull - this.firepower;
+            console.log(`alien has ${alien.hull} hull points left.`);
+            if (alien.hull <= 0) {
+
                 console.log(`Alien ship is NoMore!!`);
 
             }
         } else {
-            console.log(`We missed!`);
+            console.log(`We missed enemy!`);
         }
     }
 }
-let hero1 = new Hero ()
-hero1.attack()
-console.log(hero1)
+let player1 = new Hero ()
+// player1.attack()
+// console.log(player1)
 
-let alien = {
-
-    hull: Math.round((Math.random() * (6 - 3)) + 3),
-    firepower: Math.round((Math.random() * (4 - 2)) + 2),
-    accuracy: (Math.random() * (.8 - .6)) + .6, //for later figure out a way to make it to the hundredth?
-    isAlive: true,
-    attack(target) {
+    class Alien {
+        constructor(){
+            for (let i = 0; i < 6; i++) {
+         this.hull= Math.floor(Math.random() * 4) + 3;
+         this.firepower= Math.floor(Math.random() * 3) + 2;
+         this.accuracy= (Math.floor(Math.random() * 3) + 6) /10;
+            }
+     }  
+    attack(hero) {
         let ranNum = Math.random();
         console.log(`Accuracy is ${ranNum}`);
         if (ranNum < this.accuracy) {
-            console.log(`Gotcha!`);
-            alien.hull = alien.hull - hero1.firepower;
-            console.log(`hero has ${target.hull} hull points left.`);
-            if (alien.hull <= 0) {
-                alien.isAlive = false;
+            console.log(`shot hero ship!`);
+            hero.hull = hero.hull - this.firepower;
+            console.log(`hero has ${hero.hull} hull points left.`);
+            if (hero.hull <= 0) {
                 console.log(`You have died, hahaha!`);
 
             }
         } else {
-            console.log(`You dodged the attack!`);
+            console.log(`hero dodged the attack!`);
         }
-    }
+    }   
 }
-console.log(alien)
+let alien1 = new Alien ()
 
-let battle = (player, computer) => {
-    while (player.isAlive && computer.isAlive) {
-        player.attack(computer);
-        if (computer.isAlive) {
-            computer.attack(player);
+let aliengroup = []
+for (i = 0; i < 6; i++) {
+    aliengroup.push(new Alien('alien${i}',0, 0, 0))
+}
+console.log(aliengroup)
+let count = 0
+let currentAlien = aliengroup[""]
+
+let battle = (hero, alien) => {
+    while (player1.hull > 0 && alien.hull > 0) {
+        hero.attack(alien);
+         if (alien.hull > 0) {
+             alien.attack(hero);
         }
     }
 }
 const button= document.querySelector(".Getem")
 button.addEventListener("click", e => {
-    hero1.attack()
+    battle(player1, alien1)
 })
+    
+
+// const showRound= () => {
+    
+// }
+// showRound()
+
+// const displayHeroinfo= () => {
+
+
+// }
